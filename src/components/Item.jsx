09ca51df08942659e23c11f118d14ff3f928fa33
FrancoM <francomarquiegui@outlook.com>
2023-01-14 22:02:43 -0300

    nueva actualizacion

--- src/components/Item.jsx
+++ src/components/Item.jsx
@@ -1,25 +1,30 @@
 import axios from 'axios';
 import React, { useEffect, useState } from 'react';
 
-const Item = ({url}) => {
 
-  const [ figure, setFifure ] = useState({});
+const Item = ({ url }) => {
 
-  useEffect (() => {
+  const [figure, setFifure] = useState({});
+
+  useEffect(() => {
     axios.get(url)
-     .then(res => setFifure(res.data));
+      .then(res => setFifure(res.data));
   }, [])
-console.log(figure)
+  console.log(figure)
   return (
     <div className='box'>
-      <img src={figure.image} alt="img" />
-      <div className='info'>
 
-      <h1 className='name'>{figure.name} {""}</h1>
-      <div className='line'></div> 
-      <p><b>species:</b>{""} {figure.species} </p>
-      <p><b>name: </b>{""}{figure.origin?.name} </p>
-      <p><b>episode: </b>{""}{figure.episode?.length}</p>
+      <div className='info'>
+        <div className="img__box"> 
+        <img src={figure.image} alt="" />
+        </div>
+      <div className="color__box"> 
+        <h1 className='name'>{figure.name} {""}</h1>
+        <div className='line'></div>
+        <p><b>species:</b>{""} {figure.species} </p>
+        <p><b>name: </b>{""}{figure.origin?.name} </p>
+        <p><b>episode: </b>{""}{figure.episode?.length}</p>
+        </div>
       </div>
     </div>
   );
--- src/components/RickAndMorty.jsx
+++ src/components/RickAndMorty.jsx
@@ -4,57 +4,59 @@ import Item from './Item';
 
 
 const RickAndMorty = () => {
-  
-  const [ location, setLocation] = useState ({});
-  const [ TypeId, setTypeId ] = useState(" ");
 
-  useEffect (() => {
+  const [location, setLocation] = useState({});
+  const [TypeId, setTypeId] = useState(" ");
+
+  useEffect(() => {
     const randomLocation = Math.floor(Math.random() * 126) + 1;
     axios.get(`https://rickandmortyapi.com/api/location/${randomLocation}`)
       .then(res => setLocation(res.data));
-      
+
   }, []);
 
-  console.log(location);  
-  
-   const searchType = () => {
+  console.log(location);
+
+  const searchType = () => {
     axios.get(`https://rickandmortyapi.com/api/location/${TypeId}`)
-    .then(res => setLocation(res.data));
-   }
+      .then(res => setLocation(res.data));
+  }
 
   return (
-  <div className='container'>
-    <div className='img'></div>
-    <div className='search'>
-      <div className='seeker'> 
-       <h1>{location.name}</h1>
+    <div className='container'>
+      <div className='img'>
+       
       </div>
-      <div className='type'> 
-      <h2><b>Type:</b> {location.type}</h2>
-      <h2><b>Dimension:</b> {location.origin?.name} </h2>
-      <h2><b>population:</b> {location.residents?.length}</h2>
+      <div className='search'>
+        <div className='seeker'>
+          <h1>{location.name}</h1>
+        </div>
+        <div className='type'>
+          <h2><b>Type:</b> {location.type}</h2>
+          <h2><b>Dimension:</b> {location.dimension} </h2>
+          <h2><b>population:</b> {location.residents?.length}</h2>
 
+        </div>
+
+        <div className='button'>
+          <input className='input__search' type="text" value={TypeId} onChange={e => setTypeId(e.target.value)} />
+          <button className='button__search' onClick={searchType}>Search</button>
+        </div>
+
+
+
+        <ul className='list'>
+          {location.residents?.map(character => (
+            <Item
+              url={character}
+              key={character}
+            />
+          ))}
+        </ul>
       </div>
-     
-      <div className='button'> 
-      <input type="text" value={TypeId} onChange = {e => setTypeId(e.target.value)} />
-      <button onClick={searchType}>Search</button>
-      </div>
 
-     
 
-      <ul className='list'>
-        {location.residents?.map(character => (
-          <Item
-           url={character}
-           key={character}
-           />
-        ))}
-      </ul>
     </div>
-
-      
-  </div>
   );
 };
 
